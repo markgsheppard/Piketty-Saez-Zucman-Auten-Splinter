88e2fb94e59cb7f12cab171538e7460469d10b48
ivnagpal <63216494+ivnagpal@users.noreply.github.com>
2020-12-02 11:06:16 -0800

    commit

--- IVN_code(draft).py
+++ IVN_code(draft).py
@@ -4,120 +4,36 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import urllib
+import time
 
-from bokeh.plotting import figure, show 
+from scipy.interpolate import InterpolatedUnivariateSpline
+from scipy.interpolate import make_interp_spline, BSpline
+from scipy import interpolate
+
+from bokeh.plotting import figure, show, output_file, save
 from bokeh.layouts import gridplot
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, SingleIntervalTicker, LinearAxis
 from ipywidgets import interact
 from bokeh.io import output_notebook
 output_notebook()
 
-path = '/Users/marksheppard/Documents/GitHub/Python Project/'
-psz_url = 'http://gabriel-zucman.eu/files/PSZ2018MainData.xlsx'
-as_url = 'http://davidsplinter.com/AutenSplinter-IncomeIneq.xlsx'
 
-path = '/Users/marksheppard/Documents/GitHub/Python Project/'
+def df_merge ():
+    psz_url = 'http://gabriel-zucman.eu/files/PSZ2018MainData.xlsx'
+    as_url = 'http://davidsplinter.com/AutenSplinter-IncomeIneq.xlsx'
+    s_1 = time.time()
+    e_1 = time.time()
+    urllib.request.urlretrieve(psz_url, "piketty-saez-zucman.xlsx") 
+    urllib.request.urlretrieve(as_url, "auten-splinter.xlsx")
+    print("Data Downloaded in", (e_1 - s_1), "seconds.")
+    s_2 = time.time()
+    e_2 = time.time()
+    psz_df = pd.read_excel(psz_url, 'Data', usecols=('A:AN, AQ:BJ, BM:CY, DB:EO, EQ:FY, GA:GN, GQ:HD, HG:HR, HT:IM, IO:IP, IS:JE, JG:JO'), header=[2]).drop([0,1]).rename(columns={"Series": "Year"})
+    psz_df.drop(psz_df.tail(11).index,inplace=True)
+    as_df = pd.read_excel(as_url, 'F-A1', header=[32], usecols=('A:E, G:J')).rename(columns={'Unnamed: 0': "Year"}).drop([56,57])
+    print("Data cleaned in", (e_2 - s_2), "seconds.")
+    df = psz_df.merge(as_df, how='outer')
+    print("Merge complete")
+    return df
 
-psz_url = 'http://gabriel-zucman.eu/files/PSZ2018MainData.xlsx'
-as_url = 'http://davidsplinter.com/AutenSplinter-IncomeIneq.xlsx'
-
-psz_xls = pd.ExcelFile('piketty-saez-zucman.xlsx')
-as_xls = pd.ExcelFile("auten-splinter.xlsx")
-psz_df = pd.read_excel(psz_xls, 'Data')
-as_df = pd.read_excel(as_xls, 'F-A1')
-
-new_as_header = as_df.iloc[31] #grab the first row for the header
-as_df = as_df[32:] #take the data less the header row
-as_df.columns = new_as_header #set the header row as the df header
-as_df.columns = as_df.columns.fillna('Year')
-as_df.drop(as_df.tail(2).index,inplace=True)
-#as_df = as_df.reset_index()
-#as_df = as_df.drop(as_df.columns[6], axis=1)
-#as_df.head()
-
-new_psz_header = psz_df.iloc[1] #grab the first row for the header
-psz_df = psz_df[2:] #take the data less the header row
-psz_df.columns = new_psz_header #set the header row as the df header
-psz_df = psz_df.rename(columns={"Series": "Year"})
-psz_df = psz_df.drop([2,3])
-psz_df.drop(psz_df.tail(11).index,inplace=True)
-#psz_df.tail()
-
-psz_df.head()
-
-#as_df = as_df.filter(['Year'])
-#psz_df = psz_df.filter(['Year'])
-
-path_ = '/Users/marksheppard/Documents/GitHub/homework-3-markgsheppard-1/'
-file_name_ = [('Income.csv'),
-            ('Grant.csv')]
-income_df = pd.read_csv('Income.csv') 
-grant_df = pd.read_csv('Grant.csv')
-
-income_df = income_df.filter(['year', 'inst_name', 'faminc_mean', 'faminc_med'])
-grant_df = grant_df.filter(['year', 'inst_name', 'type_of_aid', 'number_of_students', 'average_amount', 'total_amount'])
-
-df = income_df.merge(grant_df)
-
-def grant_type(type_of_aid):
-    return df[df['type_of_aid']==type_of_aid]
-aid = df['type_of_aid'].unique()
-
-def school_type(type_of_school):
-    return df[df['inst_name']==type_of_school]
-school = df['inst_name'].unique()
-
-df['number_of_students_scaled'] = np.abs(df['number_of_students']) / 50
-
-plt.scatter('faminc_mean', 'average_amount', c = 'year', s ='number_of_students_scaled', alpha=0.6, data=df)
-plt.xlabel('Average Financial Aid Award')
-plt.ylabel('Mean Family Income')
-plt.show()
-
-plt.scatter('faminc_med', 'average_amount', c = 'year', s ='number_of_students_scaled', alpha=0.6, data=df)
-plt.xlabel('Average Financial Aid Award')
-plt.ylabel('Median Family Income')
-plt.show()
-
-plot = figure(x_axis_label='Mean Family Income', 
-              y_axis_label='Average Amount of Aid')
-
-plot.circle(df['faminc_med'], df['average_amount'])
-#https://github.com/bokeh/bokeh/blob/branch-2.3/examples/app/crossfilter/main.py
-#https://github.com/Data-and-Programming-2-TAs/Sarah-Discussion-Notebooks/blob/master/lab_5-interactive%20plotting%20example.ipynb
-
-show(plot)
-
-
-plot = figure(x_axis_label='Mean Family Income', 
-              y_axis_label='Average Amount of Aid')
-
-plot.circle(df['faminc_mean'], df['average_amount'])
-#https://github.com/bokeh/bokeh/blob/branch-2.3/examples/app/crossfilter/main.py
-#https://github.com/Data-and-Programming-2-TAs/Sarah-Discussion-Notebooks/blob/master/lab_5-interactive%20plotting%20example.ipynb
-
-show(plot)
-
-#Interactive Plots
-hover = HoverTool(tooltips = [('Median Income', '@x'), ('Amount of Aid', '@y')])
-plot = figure(x_axis_label='Median Family Income', 
-              y_axis_label='Average Amount of Aid',
-              tools=[hover])
-
-plot.circle(df['faminc_med'], df['average_amount'])
-#https://github.com/bokeh/bokeh/blob/branch-2.3/examples/app/crossfilter/main.py
-
-show(plot)
-
-@interact(School=school) # dropdown menue
-def interactive(School=school):
-    hover = HoverTool(tooltips = [('Median Income', '@x'), ('Amount of Aid', '@y')])
-    plot = figure(x_axis_label='Median Family Income', 
-              y_axis_label='Average Amount of Aid',
-              tools=[hover])
-    plot_df = school_type(School)
-    plot.circle(plot_df['faminc_med'], plot_df['average_amount'])
-    #https://github.com/bokeh/bokeh/blob/branch-2.3/examples/app/crossfilter/main.py
-    show(plot)
-
-
+df = df_merge()
